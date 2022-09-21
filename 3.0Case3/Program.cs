68a7c085dc0712fb14e42777822ff152767e59e6
adeaje <projectsmaker1803@outlook.com>
2022-09-21 08:35:12 +0200

    Case 2(Dance tournament)

---
+++ 3.0Case3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace _3._0Case3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Persons Name: ");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second Persons Name: ");
+            string prezime = Console.ReadLine();
+
+            Console.WriteLine("Age: ");
+            int age2 = Convert.ToInt32(Console.ReadLine());
+
+            string Str = ime + prezime;
+            System.IO.File.WriteAllText(@"C:\Users\adeaje\Documents\IT\1.HF\Projects I Made\C#\C# Console App(NET F)\3.0Case3\3.0Case3\Names.txt", Str);
+
+            string Age = Convert.ToString(age + age2); 
+            System.IO.File.WriteAllText(@"C:\Users\adeaje\Documents\IT\1.HF\Projects I Made\C#\C# Console App(NET F)\3.0Case3\3.0Case3\Passwords.txt", Age);
+
+            Console.WriteLine($"{ime} + {prezime}");
+            Console.WriteLine(Age);
+
+        }
+    }
+}
